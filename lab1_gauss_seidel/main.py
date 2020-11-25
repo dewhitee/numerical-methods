2e69b651238f1c3a9fda23fc047667aac59c1ac1
DewhiteE <dewhiteeofficial@gmail.com>
2020-11-25 13:22:57 +0200

    add dirs

---
+++ lab1_gauss_seidel/main.py
@@ -0,0 +1,11 @@
+print(1)
+
+f1 = lambda x, y, z: (17 - y + 2 * z) / 20
+f2 = lambda x, y, z: (-18 - 3*x + z) / 20
+f3 = lambda x, y, z: (25 - 2*x + 3*y) / 20
+
+# initial setup
+x0 = 0
+y0 = 0
+z0 = 0
+count = 1
