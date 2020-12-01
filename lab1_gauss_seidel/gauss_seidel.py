12bc2604a996f619e5256bf5c0477a4099467636
DewhiteE <dewhiteeofficial@gmail.com>
2020-12-01 15:36:53 +0200

    update gauss_seidel, implement gauss_elimination

--- lab1_gauss_seidel/gauss_seidel.py
+++ lab1_gauss_seidel/gauss_seidel.py
@@ -11,18 +11,22 @@
 # 8. Set x0 = x1, y0 = y1, z0 = z1 and so on and goto step 6
 # 9. Print value of x1, y1, z1 and so on
 # 10. Stop
-0.
-def gauss_seidel(equations: list, vars: list):
+
+def gauss_seidel(equations: list, vars: list, e: float):
     """ 
     equations -- list of lambda equations with any count of arguments.
     Example: [lambda x, y: x + y, lambda x, y: x - y]
+
     vars -- list of symbols to specify the names of the equations variables.
     Example: ['x', 'y', 'z']
+
+    e -- tolerable error
+    Example: 0.001
     """
     iteration = 1
 
     # Reading tolerable error (required accuracy)
-    e = float(input('Enter tolerable error: '))
+    #e = float(input('Enter tolerable error: '))
 
     # Implementation of Gauss Seidel Iteration
     print('\nIter', *vars, sep = "\t")
@@ -52,7 +56,7 @@ def gauss_seidel(equations: list, vars: list):
         # Checking if all current errors are greater than required error e
         condition = check_error_rate(e_list, e)
 
-    print('\nSolution')
+    print('\nSolution:')
     for (var, val) in zip(vars, vars_values):
         print(var,'= %0.3f' %(val))
 
@@ -67,7 +71,16 @@ test_equations = [
     lambda x, y, z: (25 - 2*x + 3*y) / 20
 ]
 
-gauss_seidel(test_equations, ['x','y','z'])
+gauss_seidel(test_equations, ['x','y','z'], 0.001)
+
+test_equations_2 = [
+    lambda x1, x2, x3, x4: 1/20.9*(21.70 - 1.2*x2 - 2.1*x3 - 0.9*x4),
+    lambda x1, x2, x3, x4: 1/21.2*(27.46 - 1.2*x1 - 1.5*x3 - 2.5*x4),
+    lambda x1, x2, x3, x4: 1/19.8*(28.76 - 2.1*x1 - 1.5*x2 - 1.3*x4),
+    lambda x1, x2, x3, x4: 1/32.1*(49.72 - 0.9*x1 - 2.5*x2 - 1.3*x3)
+]
+
+gauss_seidel(test_equations_2, ['x1', 'x2', 'x3', 'x4'], 0.001)
 
 
 # 1, 3, 4 var systems testing
@@ -78,4 +91,4 @@ var_1_equations = [
     lambda x1, x2, x3, x4: 1/(-7)*(-2 * x1 + x3)
 ]
 
-gauss_seidel(var_1_equations, ['x1', 'x2', 'x3', 'x4'])
+gauss_seidel(var_1_equations, ['x1', 'x2', 'x3', 'x4'], 0.001)
