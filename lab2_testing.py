a737f67eb139bfd2229795a08ae91b821a0b3e50
DewhiteE <dewhiteeofficial@gmail.com>
2020-12-06 19:10:12 +0200

    add graphical comparison of cubic-spline interpolation and least squares approximation

--- lab2_testing.py
+++ lab2_testing.py
@@ -1,11 +1,10 @@
-from scipy.interpolate import CubicSpline
-import lab2_spline_interpolation.spline_interpolation as si
-import lab2_least_squares.least_squares as ls
-from scipy import interpolate
-from scipy.interpolate import interp1d
-
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy import interpolate
+from scipy.interpolate import CubicSpline, interp1d
+
+import lab2_least_squares.least_squares as ls
+import lab2_spline_interpolation.spline_interpolation as si
 import matrix_helpers as mh
 
 vars = ['x1', 'x2', 'x3', 'x4', 'x5']
@@ -45,16 +44,16 @@ csresult = si.CubicSplineInterpolator(
 
 csresult.get_xy(
     resolution=30,
-    makeplot=True
+    makeplot=False
 )
 
 print("Matrix cond = ", mh.get_matrix_cond(csresult.matrixA))
 
-least_squares_approx_result_2 = ls.LeastSquaresApproximator(
+least_squares_approx_result_1 = ls.LeastSquaresApproximator(
     vectorX=known_vectorX_2,
     vectorY=known_vectorY_2,
     k_approx_order=2,
-    makeplot=True,
+    makeplot=False,
     ftype="auto",
 )
 
@@ -62,11 +61,56 @@ least_squares_approx_result_2 = ls.LeastSquaresApproximator(
     vectorX=known_vectorX_2,
     vectorY=known_vectorY_2,
     k_approx_order=3,
-    makeplot=True,
+    makeplot=False,
     ftype="auto",
     resolution=20
 )
 
+least_squares_approx_result_3 = ls.LeastSquaresApproximator(
+    vectorX=known_vectorX_2,
+    vectorY=known_vectorY_2,
+    k_approx_order=4,
+    makeplot=False
+)
+
+least_squares_approx_result_4 = ls.LeastSquaresApproximator(
+    vectorX=known_vectorX_2,
+    vectorY=known_vectorY_2,
+    k_approx_order=5,
+    makeplot=False
+)
+
 # ------------- LAB 2 GRAPHICAL COMPARISON
 
-
+plt.figure("Comparison of Cubic-spline interpolation with Least Squares")
+plt.title("Comparison of Cubic-spline interpolation with Least Squares")
+plt.plot(
+    csresult.get_xy()[0],
+    csresult.get_xy()[1],
+    "g--",
+    least_squares_approx_result_1.interpolated_vectorX,
+    least_squares_approx_result_1.interpolated_vectorY,
+    "r--",
+    least_squares_approx_result_2.interpolated_vectorX,
+    least_squares_approx_result_2.interpolated_vectorY,
+    "y--",
+    least_squares_approx_result_3.interpolated_vectorX,
+    least_squares_approx_result_3.interpolated_vectorY,
+    "k--",
+    least_squares_approx_result_4.interpolated_vectorX,
+    least_squares_approx_result_4.interpolated_vectorY,
+    "m--",
+    known_vectorX_2,
+    known_vectorY_2,
+    "bo")
+plt.legend(
+    [
+        "Cubic-spline interpolation",
+        "Least Squares with approximation order k = 2",
+        "Least Squares with approximation order k = 3",
+        "Least Squares with approximation order k = 4",
+        "Least Squares with approximation order k = 5",
+        "Known points"
+    ]
+)
+plt.show()
